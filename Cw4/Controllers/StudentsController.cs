e618a4d11ea32ca98d1fb880091a1336c80bcc67
Michał Pietrzak <s16560@pjwstk.edu.pl>
2020-04-05 01:41:32 +0200

    change_1

---
+++ Cw4/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw4.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok();
+        }
+    }
+}
