6f229832618056ebce20983508889d6c0b12cb83
Michał Pietrzak <s16560@pjwstk.edu.pl>
2020-04-05 20:52:51 +0200

    cwiczenia 4

--- Cw4/Controllers/StudentsController.cs
+++ Cw4/Controllers/StudentsController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cw4.Controllers
@@ -10,10 +8,85 @@ namespace Cw4.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
+        private const string ConString = "Data Source=db-mssql;Initial Catalog=s16560;Integrated Security=True";
+        
         [HttpGet]
         public IActionResult GetStudents()
         {
-            return Ok();
+            var list = new List<Student>();
+
+            using (SqlConnection con = new SqlConnection(ConString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+             
+                com.CommandText = @"select s.FirstName, s.LastName, s.BirthDate, st.Name, e.Semester
+                                        from student s
+                                        join enrollment e
+                                        on s.IdEnrollment = e.IdEnrollment
+                                        join Studies st
+                                        on e.IdStudy = st.IdStudy";
+
+                con.Open();
+                SqlDataReader dr = com.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    var st = new Student();
+               
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.BirthDate = dr["BirthDate"].ToString();
+                    st.Name = dr["Name"].ToString();
+                    st.Semester = dr["Semester"].ToString();
+
+                    list.Add(st);
+                }
+
+            }
+
+            return Ok(list);
         }
+
+        [HttpGet("{indexNumber}")]
+        public IActionResult GetStudent(string indexNumber)
+        {
+            using (SqlConnection con = new SqlConnection(ConString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                
+                com.CommandText = @"select s.FirstName, s.LastName, s.BirthDate, st.Name, e.Semester
+                                        from student s
+                                        join enrollment e
+                                        on s.IdEnrollment = e.IdEnrollment
+                                        join Studies st
+                                        on e.IdStudy = st.IdStudy
+                                        where indexNumber=@index";
+
+                com.Parameters.AddWithValue("index", indexNumber);
+
+                con.Open();
+                var dr = com.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    var st = new Student();
+
+                    st.FirstName = dr["FirstName"].ToString();
+                    st.LastName = dr["LastName"].ToString();
+                    st.BirthDate = dr["BirthDate"].ToString();
+                    st.Name = dr["Name"].ToString();
+                    st.Semester = dr["Semester"].ToString();
+
+                    return Ok(st);
+                }
+            }
+
+            return NotFound();
+        }
+       
+
     }
+
 }
